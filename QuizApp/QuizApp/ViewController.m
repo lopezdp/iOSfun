7e9e5e8a3f63ed6610119fe27caa13c245626211
LopezDP <davidplopez@live.com>
2017-11-01 22:38:15 -0400

    Added AppIcon for iOS & LaunchScreen

--- QuizApp/QuizApp/ViewController.m
+++ QuizApp/QuizApp/ViewController.m
@@ -41,12 +41,10 @@
     }
 }
 
-/*****
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-*****/
 
 /**
  Deprecated initWithName set up logic in viewDidLoad
